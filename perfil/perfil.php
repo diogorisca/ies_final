b36f2e7d6d61f9fedb6c17a7d9355a1d169d3a64
diogorisca <diogorisca@gmail.com>
2021-03-18 17:10:42 +0000

    indexstyles

--- perfil/perfil.php
+++ perfil/perfil.php
@@ -10,7 +10,7 @@
         <meta name="keywords" content="" />
         <meta name="description" content="" />
         <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-        <link href="../styles/default.css" rel="stylesheet" />
+        <link href="../styles/indexstyles.css" rel="stylesheet" />
         <link href="../styles/fonts.css" rel="stylesheet" />
     </head>
 
