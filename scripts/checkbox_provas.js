f99ddd721c15ee22c6f0b909475674dc3254c46e
astronauta <paulinofernandes@hotmail.com>
2021-03-25 15:24:37 +0000

    25/3

--- scripts/checkbox_provas.js
+++ scripts/checkbox_provas.js
@@ -11,6 +11,63 @@ function showCheckboxes() {
     }
 }
 
+
+function notaBG() {
+    var checkBox = document.getElementById("myCheck1");
+    var text = document.getElementById("text1");
+
+    if (checkBox.checked == true) {
+        text.style.display = "inline-block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function notaFQ() {
+    var checkBox = document.getElementById("myCheck2");
+    var text = document.getElementById("text2");
+
+    if (checkBox.checked == true) {
+        text.style.display = "inline-block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function notaGeometria() {
+    var checkBox = document.getElementById("myCheck12");
+    var text = document.getElementById("text12");
+
+    if (checkBox.checked == true) {
+        text.style.display = "inline-block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function notaMatA() {
+    var checkBox = document.getElementById("myCheck3");
+    var text = document.getElementById("text3");
+
+    if (checkBox.checked == true) {
+        text.style.display = "inline-block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function notaPT() {
+    var checkBox = document.getElementById("myCheck4");
+    var text = document.getElementById("text4");
+
+    if (checkBox.checked == true) {
+        text.style.display = "inline-block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+
 /*
 function notaAlemao() {
     // Get the checkbox
@@ -25,20 +82,7 @@ function notaAlemao() {
         text.style.display = "none";
     }
 }
-*/
 
-function notaBG() {
-    var checkBox = document.getElementById("myCheck1");
-    var text = document.getElementById("text1");
-
-    if (checkBox.checked == true) {
-        text.style.display = "inline-block";
-    } else {
-        text.style.display = "none";
-    }
-}
-
-/*
 function notaDesenho() {
     var checkBox = document.getElementById("myCheck6");
     var text = document.getElementById("text6");
@@ -82,19 +126,7 @@ function notaFilosofia() {
         text.style.display = "none";
     }
 }
-*/
 
-function notaFQ() {
-    var checkBox = document.getElementById("myCheck2");
-    var text = document.getElementById("text2");
-
-    if (checkBox.checked == true) {
-        text.style.display = "inline-block";
-    } else {
-        text.style.display = "none";
-    }
-}
-/*
 function notaFrances() {
     var checkBox = document.getElementById("myCheck10");
     var text = document.getElementById("text10");
@@ -109,17 +141,6 @@ function notaFrances() {
 function notaGeografia() {
     var checkBox = document.getElementById("myCheck11");
     var text = document.getElementById("text11");
-
-    if (checkBox.checked == true) {
-        text.style.display = "inline-block";
-    } else {
-        text.style.display = "none";
-    }
-}
-
-function notaGeometria() {
-    var checkBox = document.getElementById("myCheck12");
-    var text = document.getElementById("text12");
 
     if (checkBox.checked == true) {
         text.style.display = "inline-block";
@@ -182,18 +203,7 @@ function notaLP() {
         text.style.display = "none";
     }
 }
-*/
-function notaMatA() {
-    var checkBox = document.getElementById("myCheck3");
-    var text = document.getElementById("text3");
 
-    if (checkBox.checked == true) {
-        text.style.display = "inline-block";
-    } else {
-        text.style.display = "none";
-    }
-}
-/*
 function notaMatB() {
     var checkBox = document.getElementById("myCheck18");
     var text = document.getElementById("text18");
@@ -215,18 +225,7 @@ function notaMatAplicada() {
         text.style.display = "none";
     }
 }
-*/
-function notaPT() {
-    var checkBox = document.getElementById("myCheck4");
-    var text = document.getElementById("text4");
 
-    if (checkBox.checked == true) {
-        text.style.display = "inline-block";
-    } else {
-        text.style.display = "none";
-    }
-}
-/*
 function notaMandarim() {
     var checkBox = document.getElementById("myCheck20");
     var text = document.getElementById("text20");
