9aea73dc0616ed26f05f414501ca2a281c7003d2
astronauta <paulinofernandes@hotmail.com>
2021-03-27 12:30:59 +0000

    algoritmos explicados - a funcionar

--- listas/listar_info_curso.php
+++ listas/listar_info_curso.php
@@ -9,3 +9,11 @@ $sql = "SELECT * FROM curso WHERE nome = '$nome' and faculdade = '$faculdade'";
 
 
 */
+
+
+/*
+
+Nesta pagina devera existir um botao que diga simular candidatura, assim que o utilizador o prima, chama a função
+simulaçao e da display de uma tabela com a ordem de suposta entrada para o curso.
+
+*/
