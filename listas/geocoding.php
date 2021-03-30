89ff2532519f5053d31d79fc8fd704f67ba465b4
astronauta <paulinofernandes@hotmail.com>
2021-03-30 17:30:34 +0100

    geocoding - a funcionar

--- listas/geocoding.php
+++ listas/geocoding.php
@@ -1,7 +1,9 @@
-*/
+<html>
+<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=angkorwat&
+key=AIzaSyCde12RAX3KGlMl0X7E29Hu8weTOT9s_I0">
+</iframe>
 
-AIzaSyDLuKhcnPtANnQAs45SPSkP6QMsoIFyZ6o
+</html>
 
 
-*/
-
+SELECT morada FROM ies WHERE id=1
