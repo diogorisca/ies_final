20bbf3c464cf481c9451421bb379db4c052c439b
astronauta <paulinofernandes@hotmail.com>
2021-03-31 15:36:02 +0100

    31/3

--- listas/geocoding.php
+++ listas/geocoding.php
@@ -1,9 +1,8 @@
 <html>
-<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=angkorwat&
+<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=UniversidadeLusofonaPorto&
 key=AIzaSyCde12RAX3KGlMl0X7E29Hu8weTOT9s_I0">
 </iframe>
 
 </html>
 
 
-SELECT morada FROM ies WHERE id=1
